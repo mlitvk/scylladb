0b77aab3e67ae4896f10e316b63aa90ae7c6de12
Michael Litvak <michael.litvak@scylladb.com>
2026-05-09 12:13:09 +0200

    logstor: format files on startup
    add a config option to format logstor files on startup. previously the
    files are created and formatted lazily on the first access. this option
    allows to create and format all files before the first use in order to
    get better write performance on first access.

--- db/config.hh
+++ db/config.hh
@@ -275,6 +275,7 @@ public:
     named_value<uint32_t> logstor_file_size_in_mb;
     named_value<uint32_t> logstor_compaction_trigger_threshold_percent;
     named_value<uint32_t> logstor_compaction_soft_pressure_threshold_percent;
+    named_value<bool> logstor_format_on_startup;
     named_value<uint32_t> file_cache_size_in_mb;
     named_value<uint32_t> memtable_flush_queue_size;
     named_value<uint32_t> memtable_flush_writers;
