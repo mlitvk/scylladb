eb4b7a5cb1476f14765ca281eb196b1f54ec3a37
Michael Litvak <michael.litvak@scylladb.com>
2026-06-16 11:50:15 +0200

    logstor: ignore compaction completion result
    when awaiting the compaction fiber in stop_ongoing_compactions,
    disable_compaction, etc, ignore the result of the compaction, since we
    only want to wait for the compaction to complete and it doesn't matter
    if it failed.

--- replica/logstor/segment_manager.cc
+++ replica/logstor/segment_manager.cc
@@ -1318,7 +1318,7 @@ void compaction_manager_impl::submit(logstor_group& cg) {
     state.as = {};
     state.completion = shared_future(with_gate(_async_gate,
         [this, &cg, &state] -> future<> {
-            return do_compact(cg, state.as).then([&state] {
+            return do_compact(cg, state.as).finally([&state] {
                 state.running = false;
             });
         }
@@ -1331,7 +1331,12 @@ future<> compaction_manager_impl::stop_ongoing_compactions(logstor_group& cg) {
         co_return;
     }
     state->as.request_abort();
-    co_await state->completion.get_future();
+    try {
+        co_await state->completion.get_future();
+    } catch (...) {
+        logstor_logger.warn("Stopping ongoing logstor compactions for table {} failed: {}. Ignored",
+                cg.table_id(), std::current_exception());
+    }
 }
 
 future<> compaction_manager_impl::remove(logstor_group& cg) {
@@ -1345,7 +1350,12 @@ future<compaction_reenabler> compaction_manager_impl::disable_compaction(logstor
     ++state.compaction_disabled_counter;
 
     // Wait for any ongoing compaction to finish before disabling
-    co_await state.completion.get_future();
+    try {
+        co_await state.completion.get_future();
+    } catch (...) {
+        logstor_logger.warn("Disabling logstor compaction for table {} failed while waiting for ongoing compaction: {}. Ignored",
+                cg.table_id(), std::current_exception());
+    }
 
     co_return compaction_reenabler([this, &cg] {
         auto it = _groups.find(&cg);
